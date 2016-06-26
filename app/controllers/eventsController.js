66796ba26a1fa377998b9fb53ea79bc0deb845a8
Maxi Britez <maxi.britez@redb.ee>
2016-06-26 13:59:46 -0300

    Remove hardcode events

--- app/controllers/eventsController.js
+++ app/controllers/eventsController.js
@@ -40,48 +40,6 @@ app.controller(
 
             $scope.eventId = $routeParams.eventId;
 
-            $scope.eventItems = [
-                {
-                    id: 1,
-                    name:"Bodas",
-                    icon:"bodas"
-                }, 
-                {
-                    id: 2,
-                    name:"Cumpleaños",
-                    icon:"cumpleanos"
-                }, 
-                {
-                    id: 3,
-                    name:"Quinceañeras",
-                    icon:"quinceaneras"
-                }, 
-                {
-                    id: 4,
-                    name:"Graduaciones",
-                    icon:"graduaciones"
-                }, 
-                {
-                    id: 7,
-                    name:"Bautizos",
-                    icon:"bautizo"
-                }, 
-                {
-                    id: 8,
-                    name:"Conferencias",
-                    icon:"conferencias"
-                }, 
-                {
-                    id: 9,
-                    name:"Showers",
-                    icon:"showers"
-                }, 
-                {
-                    id: 10,
-                    name:"Otros",
-                    icon:"icon_otros"
-                }
-            ];
         }
     ]
 );
