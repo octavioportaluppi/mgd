4a88245ee05028358a6eec294897fd95b5a3552c
Martin Britez <martin.britez@redb.ee>
2016-06-29 12:17:38 -0300

    Add icon filter

--- app.js
+++ app.js
@@ -103,28 +103,52 @@ app.filter('tel', function () {
 
 
 
-    .filter('removeAcentos', function(){
+.filter('icon', function(){
     return function (source) {
-        if(!angular.isDefined(source)){ return; }
-
-        var accent = [
-                /[\300-\306]/g, /[\340-\346]/g, // A, a
-                /[\310-\313]/g, /[\350-\353]/g, // E, e
-                /[\314-\317]/g, /[\354-\357]/g, // I, i
-                /[\322-\330]/g, /[\362-\370]/g, // O, o
-                /[\331-\334]/g, /[\371-\374]/g, // U, u
-                /[\321]/g, /[\361]/g, // N, n
-                /[\307]/g, /[\347]/g, // C, c
-            ],
-            noaccent = ['A','a','E','e','I','i','O','o','U','u','N','n','C','c'];
-
-        for (var i = 0; i < accent.length; i++){
-            source = source.replace(accent[i], noaccent[i]);
+        if(!angular.isDefined(source)){
+            return;
         }
 
-        return source;
+        source = source.toLowerCase();
+
+        var values = [
+            {
+                id:'á',
+                replace: 'a'
+            },
+            {
+                id:'é',
+                replace: 'e'
+            },
+            {
+                id:'í',
+                replace: 'i'
+            },
+            {
+                id:'ó',
+                replace: 'o'
+            },
+            {
+                id:'ú',
+                replace: 'u'
+            },
+            {
+                id:'ñ',
+                replace: 'n'
+            },
+            {
+                id:' ',
+                replace: '-'
+            }
+        ];
+
+        values.forEach(function(value){
+           source = source.split(value.id).join(value.replace);
+        });
+
+        return 'icon-' + source;
     };
-})
+});
 
 
 
