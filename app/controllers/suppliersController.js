e15094234db1329e1990095d6f0db4ec8082f1b8
Martin Britez <martin.britez@redb.ee>
2016-07-12 18:02:51 -0300

    supplier-detail slide

--- app/controllers/suppliersController.js
+++ app/controllers/suppliersController.js
@@ -1,57 +1,31 @@
 ﻿'use strict';
 app.controller('suppliersController',
-    ['$scope', 'supplierService', '$routeParams','ngAuthSettings',
-        function ($scope, supplierService, $routeParams,ngAuthSettings) {
-
-        $scope.supplier = {};
-
-        supplierService
-            .getSuppliersById($routeParams.supplierId)
-            .then(function(res){
-                $scope.supplier = res.data;
-                if ($scope.supplier.LogoId > 0)
-                $scope.supplier.LogoUrl = ngAuthSettings.apiServiceBaseUri + '/api/Pictures/' +   $scope.supplier.LogoId + '/Image';
-                $scope.supplier.Pictures = res.data.Pictures
-                .map(function (pic) {
-                    return ngAuthSettings.apiServiceBaseUri + '/api/Pictures/' + pic.Id + '/Image';
-                });
-
-            });
-
-
-
-
-
-
-
-
+    ['$scope', 'supplierService', '$routeParams', 'ngAuthSettings',
+        function ($scope, supplierService, $routeParams, ngAuthSettings) {
+            $scope.supplier = {};
             $scope.isSupplier = false;
             $scope.myInterval = 3000;
             $scope.noWrapSlides = false;
 
             $scope.active = 0;
+            $scope.slides = [];
 
-            $scope.slides = [{
-                id: 0,
-                img: 'img/mi_gran_dia_conferencia.jpg',
-                text: 'Te ayudamos a organizar tus conferencias'
-            }, {
-                id: 1,
-                img: 'img/mi_gran_dia_cumpleanos.jpg',
-                text: 'Te ayudamos a organizar tus cumpleaños'
-            }, {
-                id: 2,
-                img: 'img/mi_gran_dia_boda.jpg',
-                text: 'Te ayudamos a organizar tu boda'
-            }];
 
             supplierService
-                .getEvents()
-                .then(function(response) {
-                    $scope.eventItems = response.data;
-                });
+                .getSuppliersById($routeParams.supplierId)
+                .then(function (res) {
+                    $scope.supplier = res.data;
+                    if ($scope.supplier.LogoId > 0)
+                        $scope.supplier.LogoUrl = ngAuthSettings.apiServiceBaseUri + 'api/Pictures/' + $scope.supplier.LogoId + '/Image';
+                    var id = 0;
+                    res.data.Pictures
+                        .forEach(function (pic) {
+                            var elem = {
+                                img: ngAuthSettings.apiServiceBaseUri + 'api/Pictures/' + pic.Id + '/Image',
+                                id: id++
+                            };
 
-            $scope.getEvents = function (){
-                return $scope.eventItems;
-            }
-}]);
+                            $scope.slides.push(elem)
+                        });
+                });
+        }]);
