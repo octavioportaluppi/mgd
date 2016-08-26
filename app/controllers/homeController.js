32654e48e47128fa9eb7a31cd433b982f62e6f32
abrahamhinojo <abraham.hinojo@gmail.com>
2016-08-26 10:18:52 -0700

    avances en el home

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,11 +1,18 @@
 ﻿'use strict';
-app.controller('homeController', ['$scope', 'supplierService', function ($scope, supplierService) {
+app.controller('homeController', ['$scope', 'supplierService', 'ngAuthSettings', function ($scope, supplierService, ngAuthSettings) {
 	$scope.isSupplier = false;
 	$scope.myInterval = 3000;
 	$scope.noWrapSlides = false;
 
+	// Suppliers
+	$scope.size = 5;
+	$scope.page = 1;
+	$scope.filter = $scope.filter || undefined;
+	$scope.query = $scope.query || undefined;
+	$scope.suppliers = [];
+
 	$scope.active = 0;
-	
+
 	$scope.slides = [{
 		id: 0,
 		img: 'img/mi_gran_dia_conferencia.jpg',
@@ -29,5 +36,34 @@ app.controller('homeController', ['$scope', 'supplierService', function ($scope,
 	$scope.getEvents = function (){
 		return $scope.eventItems;
 	}
-   
+
+	$scope.getSuppliers = function (){
+			$scope.loading = true;
+			supplierService
+					.getAllSuppliers(
+					$scope.size,
+					$scope.page,
+					$scope.filter,
+					$scope.query,
+					$scope.CityId)
+					.success(function (response){
+							$scope.loading = false;
+							$scope.totalSuppliers = response.TotalResults;
+							$scope.filters = response.QueryFilterInfo;
+							$scope.totalPages = response.TotalPages;
+							$scope.suppliers = response.Content;
+							$scope.suppliers.forEach(function(supplier){
+									if (supplier.LogoId > 0)
+											supplier.LogoUrl = ngAuthSettings.apiServiceBaseUri + '/api/Pictures/' + supplier.LogoId + '/Image';
+							});
+						$scope.suppliers = $scope.suppliers.slice(0,4);
+					});
+	};
+
+	$scope.initHome = function() {
+		$scope.getSuppliers();
+	}
+
+	$scope.initHome();
+
 }]);
